7727b19191bdd1e62cf10a5374071d29f0d97950
Juraj Hlista <jurajhlista@gmail.com>
2019-03-08 22:35:13 +0700

    Add manager for miner instances
    The manager checks if miner nonces are exhausted by miner instances
    and distributes miner nonces so they do not overlap.

---
+++ src/aestratum_client_generator_manager.erl
@@ -0,0 +1,142 @@
+-module(aestratum_client_generator_manager).
+
+-behaviour(gen_server).
+
+%% API.
+-export([start_link/0,
+         stop/0,
+         add/3,
+         del/1,
+         generate/2
+        ]).
+
+%% gen_server.
+-export([init/1,
+         handle_call/3,
+         handle_cast/2
+        ]).
+
+-type id()            :: aestratum_client_miner:id().
+
+-type repeats()       :: aestratum_miner:repeats().
+
+-type extra_nonce()   :: aestratum_nonce:part_nonce().
+
+-type worker_count()  :: non_neg_integer().
+
+-type stats()         :: #{started => {worker_count(), repeats()},
+                           queued  => {worker_count(), repeats()}}.
+
+-record(state, {
+          workers     :: orddict:orddict(id(), {pid(), repeats()})
+         }).
+
+-define(SERVER, ?MODULE).
+
+%% API.
+
+-spec start_link() -> {ok, pid()}.
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+-spec stop() -> ok.
+stop() ->
+    gen_server:stop(?SERVER).
+
+-spec add(id(), pid(), repeats()) -> ok.
+add(Id, Pid, Repeats) when
+      is_integer(Id) and is_pid(Pid) and
+      (is_integer(Repeats) and (Repeats > 0)) ->
+    gen_server:call(?SERVER, {add, Id, Pid, Repeats}).
+
+-spec del(id()) -> ok.
+del(Id) when is_integer(Id) ->
+    gen_server:call(?SERVER, {del, Id}).
+
+%% TODO
+-spec generate(term(), extra_nonce()) ->
+    {ok, stats()} | {error, miner_nonce_exhausted}.
+generate(Job, ExtraNonce) ->
+    gen_server:call(?SERVER, {generate, Job, ExtraNonce}).
+
+%% gen_server callbacks.
+
+init([]) ->
+    {ok, #state{workers = orddict:new()}}.
+
+handle_call({add, Id, Pid, Repeats}, _From, #state{workers = Workers} = State) ->
+    State1 = State#state{workers = orddict:store(Id, {Pid, Repeats}, Workers)},
+    {reply, ok, State1};
+handle_call({del, Id}, _From, #state{workers = Workers} = State) ->
+    {reply, ok, State#state{workers = orddict:erase(Id, Workers)}};
+handle_call({generate, Job, ExtraNonce}, _From, State) ->
+    {ok, Reply, State1} = handle_generate(Job, ExtraNonce, State),
+    {reply, Reply, State1}.
+
+handle_cast(_Req, State) ->
+    {noreply, State}.
+
+%% Internal functions.
+
+handle_generate(Job, ExtraNonce, #state{workers = Workers} = State) ->
+    Workers1 = orddict:to_list(Workers),
+    MinerNonceNBytes = aestratum_nonce:complement_nbytes(ExtraNonce),
+    MaxMinerNonceVal = aestratum_nonce:max(MinerNonceNBytes),
+    MaxRepeats = max_repeats(Workers1),
+    %% For example, 16#ff is 255 which is the max miner nonce value. It ranges
+    %% from 0 to 255, it covers 256 miner nonce value (repeats).
+    case (MaxMinerNonceVal + 1) - MaxRepeats of
+        N when N >= 0 ->
+            %% N is a max number that can be used as an initial miner nonce
+            %% value in order not to cause nonce overflow.
+            MinerNonceVal = random_miner_nonce_value(N),
+            MinerNonce = aestratum_nonce:new(miner, MinerNonceVal, MinerNonceNBytes),
+            Stats = #{started => {0, 0}, queued => {0, 0}},
+            %% Shuffle the worker list so the workers are assigned random
+            %% miner nonces.
+            Workers2 = shuffle(Workers1),
+            Reply = generate(Workers2, Job, ExtraNonce, MinerNonce, Stats),
+            {ok, {ok, Reply}, State};
+        _Other ->
+            %% The workers cover more repeats than there are miner nonces.
+            %% The workers could still start mining, some could mine the same
+            %% nonces. This is unlikely to happen though. Therefore, this case
+            %% is not handled as it possibly should be, and just error is
+            %% returned.
+            %% TODO: ^ low priority.
+            {ok, {error, miner_nonce_exhausted}, State}
+    end.
+
+%% This function is only called when the miner nonce values are not exhausted.
+generate([{_Id, {Pid, Repeats}} | Workers], Job, ExtraNonce, MinerNonce, Stats) ->
+    {Action, Repeats} = send_job(Pid, Job, ExtraNonce, MinerNonce),
+    MinerNonceVal = aestratum_nonce:value(MinerNonce) + Repeats - 1,
+    MinerNonce1 = aestratum_nonce:update(MinerNonceVal, MinerNonce),
+    Stats1 = update_stats(Action, Repeats, Stats),
+    generate(Workers, Job, ExtraNonce, MinerNonce1, Stats1);
+generate([], _Job, _ExtraNonce, _MinerNonce, Stats) ->
+    Stats.
+
+max_repeats(Workers) ->
+    lists:foldl(fun({_Id, {_Pid, Repeats}}, Count) ->
+                        Count + Repeats
+                end, 0, Workers).
+
+send_job(Pid, Job, ExtraNonce, MinerNonce) ->
+    aestratum_client_generator_worker:generate(Pid, Job, ExtraNonce, MinerNonce).
+
+random_miner_nonce_value(Max) ->
+    rand:uniform(Max + 1) - 1.
+
+shuffle([]) ->
+    [];
+shuffle([_X] = L) ->
+    L;
+shuffle(L) ->
+    [X || {_,X} <- lists:sort([{rand:uniform(), N} || N <- L])].
+
+update_stats(started, Repeats, #{started := {WorkerCount, NonceCount}} = Stats) ->
+    Stats#{started => {WorkerCount + 1, NonceCount + Repeats}};
+update_stats(queued, Repeats, #{queued := {WorkerCount, NonceCount}} = Stats) ->
+    Stats#{queued => {WorkerCount + 1, NonceCount + Repeats}}.
+
---
+++ test/aestratum_client_generator_manager_tests.erl
@@ -0,0 +1,133 @@
+-module(aestratum_client_generator_manager_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(TEST_MODULE, aestratum_client_generator_manager).
+-define(NONCE_MODULE, aestratum_nonce).
+-define(CLIENT_GENERATOR_WORKER_MODULE, aestratum_client_generator_worker).
+
+-define(TEST_JOB_ID, <<"0102030405060708">>).
+-define(TEST_TARGET, 16#ff0000000000000000000000000000000000000000000000000000000).
+-define(TEST_BLOCK_HASH,
+        <<"000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f">>).
+
+client_generator_manager_test_() ->
+    {foreach,
+     fun() ->
+             meck:new(?CLIENT_GENERATOR_WORKER_MODULE, [passthrough]),
+             {ok, Pid} = ?TEST_MODULE:start_link(),
+             Pid
+     end,
+     fun(_) ->
+             ok = ?TEST_MODULE:stop(),
+             meck:unload(?CLIENT_GENERATOR_WORKER_MODULE)
+     end,
+     [fun(_) -> add_and_del_worker() end,
+      fun(_) -> generate(no_workers) end,
+      fun(_) -> generate(single_worker, miner_nonce_exhausted) end,
+      fun(_) -> generate(single_worker, started) end,
+      fun(_) -> generate(single_worker, queued) end,
+      fun(_) -> generate(multiple_workers, miner_nonce_exhausted) end,
+      fun(_) -> generate(multiple_workers, started) end,
+      fun(_) -> generate(multiple_workers, queued) end,
+      fun(_) -> generate(multiple_workers, started_and_queued) end]}.
+
+add_and_del_worker() ->
+    [?_assertEqual(ok, ?TEST_MODULE:add(0, new_pid(), 1)),
+     ?_assertEqual(ok, ?TEST_MODULE:del(0))].
+
+generate(no_workers) ->
+    T = <<"generate - no_workers">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 1, target => ?TEST_TARGET, empty_queue => true},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 16#112233445566, 6),
+    [{T, ?_assertEqual({ok, #{started => {0, 0}, queued => {0, 0}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}].
+
+generate(single_worker, miner_nonce_exhausted) ->
+    T = <<"generate - single_worker, miner_nonce_exhausted">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 1, target => ?TEST_TARGET, empty_queue => true},
+    %% Extra nonce takes up 7 bytes, only 1 byte is left for a worker.
+    %% 1 byte are nonce values between 0 and 255 (so 256 nonce values).
+    ExtraNonce = ?NONCE_MODULE:new(extra, 16#aabbccddeeff00, 7),
+    prep_workers([{0, new_pid(), 257, available}]),
+    [{T, ?_assertEqual({error, miner_nonce_exhausted},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(single_worker, started) ->
+    T = <<"generate - single_worker, started">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 2, target => ?TEST_TARGET, empty_queue => false},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 16#aabbccdd, 7),
+    prep_workers([{0, new_pid(), 256, available}]),
+    [{T, ?_assertEqual({ok, #{started => {1, 256}, queued => {0, 0}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(single_worker, queued) ->
+    T = <<"generate - single_worker, queued">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 2, target => ?TEST_TARGET, empty_queue => false},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 16#ffffffffffff, 6),
+    prep_workers([{0, new_pid(), 1, busy}]),
+    [{T, ?_assertEqual({ok, #{started => {0, 0}, queued => {1, 1}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(multiple_workers, miner_nonce_exhausted) ->
+    T = <<"generate - multiple_workers, miner_nonce_exhausted">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 1, target => ?TEST_TARGET, empty_queue => true},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 16#11223344556677, 7),
+    prep_workers([{0, new_pid(), 100, available},
+                  {1, new_pid(), 100, busy},
+                  {2, new_pid(), 57, available}]),
+    [{T, ?_assertEqual({error, miner_nonce_exhausted},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(multiple_workers, started) ->
+    T = <<"generate - multiple_workers, started">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 2, target => ?TEST_TARGET, empty_queue => true},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 0, 4),
+    prep_workers([{0, new_pid(), 10, available},
+                  {1, new_pid(), 100, available},
+                  {2, new_pid(), 50, available}]),
+    [{T, ?_assertEqual({ok, #{started => {3, 160}, queued => {0, 0}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(multiple_workers, queued) ->
+    T = <<"generate - multiple_workers, queued">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 2, target => ?TEST_TARGET, empty_queue => false},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 0, 4),
+    prep_workers([{0, new_pid(), 10, busy},
+                  {1, new_pid(), 100, busy},
+                  {2, new_pid(), 50, busy}]),
+    [{T, ?_assertEqual({ok, #{started => {0, 0}, queued => {3, 160}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}];
+generate(multiple_workers, started_and_queued) ->
+    T = <<"generate - multiple_workers, started_and_queued">>,
+    Job = #{job_id => ?TEST_JOB_ID, block_hash => ?TEST_BLOCK_HASH,
+            block_version => 2, target => ?TEST_TARGET, empty_queue => false},
+    ExtraNonce = ?NONCE_MODULE:new(extra, 10000, 3),
+    prep_workers([{0, new_pid(), 1111, busy},
+                  {1, new_pid(), 2222, available},
+                  {2, new_pid(), 3333, busy}]),
+    [{T, ?_assertEqual({ok, #{started => {1, 2222}, queued => {2, 4444}}},
+                       ?TEST_MODULE:generate(Job, ExtraNonce))}].
+
+prep_workers(Workers) ->
+    prep_workers(Workers, Workers).
+
+prep_workers([{Id, Pid, Repeats, _Status} | Rest], Workers) ->
+    ok = ?TEST_MODULE:add(Id, Pid, Repeats),
+    prep_workers(Rest, Workers);
+prep_workers([], Workers) ->
+    meck:expect(?CLIENT_GENERATOR_WORKER_MODULE, generate,
+                fun(Pid, _, _, _) ->
+                        {Id, Pid, Repeats, Status} = lists:keyfind(Pid, 2, Workers),
+                        {worker_action(Status), Repeats}
+                end).
+
+worker_action(available) -> started;
+worker_action(busy)      -> queued.
+
+%% This is just to get a PID. The process itself is not important.
+new_pid() ->
+    spawn(fun() -> ok end).
+
